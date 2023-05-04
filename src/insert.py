46dc0a58f1a615bba7beda7acf855c1c8da30e0e
Thiago Lopes <thiago.lopes.dev@gmail.com>
2023-05-03 23:05:22 -0300

    fix: put nullable elements

--- src/insert.py
+++ src/insert.py
@@ -18,8 +18,7 @@ password = "starship_password",
 port = "5432"
 
 path = "/mnt/c/Users/Thiago/Downloads/DADOS RECEITA/Estabelecimentos"
-csv_files = [f"{path}/K3241.K03200Y1.D30408.csv",
-             f"{path}/K3241.K03200Y2.D30408.csv"]
+csv_files = [f"{path}/K3241.K03200Y2.D30408.csv"]
 
 
 # Define a base para as classes de modelo
@@ -30,36 +29,36 @@ class Establishment(Base):
     __tablename__ = 'establishment'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    taxpayerRegistry = Column(String)
-    cnpjOrder = Column(String)
-    cnpjDv = Column(String)
-    branchIdentifier = Column(String)
-    fantasyName = Column(String)
-    cadastralSituation = Column(String)
-    dateCadastralSituation = Column(String)
-    reasonCadastralSituation = Column(String)
-    outsideCityName = Column(String)
-    country = Column(String)
-    startDateActivity = Column(String)
-    principalCNAEFiscal = Column(String)
-    secondaryCNAEFiscal = Column(String)
-    typeOfStreet = Column(String)
-    street = Column(String)
-    number = Column(String)
-    complement = Column(String)
-    neighborhood = Column(String)
-    cep = Column(String)
-    UF = Column(String)
-    city = Column(String)
-    ddd1 = Column(String)
-    phone1 = Column(String)
-    ddd2 = Column(String)
-    phone2 = Column(String)
-    faxDDD = Column(String)
-    fax = Column(String)
-    email = Column(String)
-    specialSituation = Column(String)
-    dateSpecialSituation = Column(String)
+    taxpayerRegistry = Column(String, unique=True, nullable=True)
+    cnpjOrder = Column(String, nullable=True)
+    cnpjDv = Column(String, nullable=True)
+    branchIdentifier = Column(String, nullable=True)
+    fantasyName = Column(String, nullable=True)
+    cadastralSituation = Column(String, nullable=True)
+    dateCadastralSituation = Column(String, nullable=True)
+    reasonCadastralSituation = Column(String, nullable=True)
+    outsideCityName = Column(String, nullable=True)
+    country = Column(String, nullable=True)
+    startDateActivity = Column(String, nullable=True)
+    principalCNAEFiscal = Column(String, nullable=True)
+    secondaryCNAEFiscal = Column(String, nullable=True)
+    typeOfStreet = Column(String, nullable=True)
+    street = Column(String, nullable=True)
+    number = Column(String, nullable=True)
+    complement = Column(String, nullable=True)
+    neighborhood = Column(String, nullable=True)
+    cep = Column(String, nullable=True)
+    UF = Column(String, nullable=True)
+    city = Column(String, nullable=True)
+    ddd1 = Column(String, nullable=True)
+    phone1 = Column(String, nullable=True)
+    ddd2 = Column(String, nullable=True)
+    phone2 = Column(String, nullable=True)
+    faxDDD = Column(String, nullable=True)
+    fax = Column(String, nullable=True)
+    email = Column(String, nullable=True)
+    specialSituation = Column(String, nullable=True)
+    dateSpecialSituation = Column(String, nullable=True)
 
 
 # Cria uma engine para conectar ao banco de dados
